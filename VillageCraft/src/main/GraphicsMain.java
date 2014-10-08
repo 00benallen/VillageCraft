82054a2b22cfb857ab26b6c6c06c41e0766da0a1
Ben Pinhorn <00ben.allen@gmail.com>
2014-10-08 09:50:19 -0400

    Window made

--- VillageCraft/src/main/GraphicsMain.java
+++ VillageCraft/src/main/GraphicsMain.java
@@ -1,5 +1,24 @@
 package main;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class GraphicsMain {
+	private static JFrame frame;
+	public static final int WIDTH = 1024, HEIGHT = 768;
+	
+	public static void init() {
+		frame = new JFrame();
+	}
+
+	public static void start() {
+		 frame = new JFrame("VillageCraft");
+	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	     frame.setResizable(false);
+	     frame.pack();
+	     frame.setVisible(true);
+	}
 
 }
