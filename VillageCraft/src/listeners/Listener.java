eef9142e53b7c35276e49f4d6785eee3bafccdf4
Daniel <Daniel@DanielMcIntosh.ca>
2014-10-08 12:56:59 -0400

    working on Listener class

--- VillageCraft/src/listeners/Listener.java
+++ VillageCraft/src/listeners/Listener.java
@@ -1,5 +1,55 @@
 package listeners;
 
-public class Listener {
+import java.awt.event.*;
+
+public class Listener implements KeyListener, MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
