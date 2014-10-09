314677b7d92b2a3b3876eb58eabfd65ae007ee2d
Daniel <Daniel@DanielMcIntosh.ca>
2014-10-08 20:28:19 -0400

    Finished work on the Listener class

--- VillageCraft/src/listeners/Listener.java
+++ VillageCraft/src/listeners/Listener.java
@@ -1,11 +1,18 @@
 package listeners;
 
 import java.awt.event.*;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
-public class Listener implements KeyListener, MouseListener{
+public class Listener implements KeyListener, MouseListener, MouseMotionListener{
 	ArrayList<InputEvent> unprocessedEvents = new ArrayList<InputEvent>();
+	Point2D mouse = new Point2D.Double(-1, -1);
 
+	public Point2D getMouse()
+	{
+		return (Point2D) mouse.clone();
+	}
+	
 	public InputEvent getEvent(int index)
 	{
 		InputEvent e = unprocessedEvents.remove(index); 
@@ -19,22 +26,36 @@ public class Listener implements KeyListener, MouseListener{
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		unprocessedEvents.add(e);
+		mouse = e.getPoint();
 	}
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		unprocessedEvents.add(e);
+		mouse = e.getPoint();
 	}
 	@Override
 	public void mouseExited(MouseEvent e) {
 		unprocessedEvents.add(e);
+		mouse = new Point2D.Double(-1, -1);
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
 		unprocessedEvents.add(e);
+		mouse = e.getPoint();
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		unprocessedEvents.add(e);
+		mouse = e.getPoint();
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		unprocessedEvents.add(e);
+		mouse = e.getPoint();
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		mouse = e.getPoint();
 	}
 
 	@Override
