a3ffa1ff89d44530319c04871d7a6461cb8b0de2
Daniel McIntosh <Daniel@DanielMcIntosh.ca>
2014-10-10 17:18:19 -0400

    Worked on Material class, have a look at it, because I'm not 100% sure whether this is the way we should do it yet.

--- VillageCraft/src/resources/Chunk.java
+++ VillageCraft/src/resources/Chunk.java
@@ -1,8 +1,18 @@
 package resources;
 
+import java.util.ArrayList;
+
 public class Chunk {
 	private int biome;
+	public static final int BIOME_BARREN = 0, BIOME_FOREST = 1;
 	
+	private ArrayList<Material> resources = new ArrayList<Material>();
+	//TODO fill this in
+	//public static final int[] biomeRsrceType = {Material.RSRCE_NOTHING, Material.RSRCE_LUMBER};
+	public static final Material[] biomeRsrce = {null, Material.lumber};
+	
+	//TODO decide what to make this
+	public static final int initResources = 0;
 	public Chunk(int biome) {
 		this.setBiome(biome);
 	}
@@ -13,6 +23,20 @@ public class Chunk {
 	public void setBiome(int biome) {
 		this.biome = biome;
 	}
+	
+	public int getNumRsrces() {
+		return resources.size();
+	}
+	public ArrayList<Material> harvest(int quantity)
+	{
+		ArrayList<Material> harvested = new ArrayList<Material>();
+		for (int i = 0; i < quantity; ++i)
+		{
+			harvested.add(resources.remove(0));
+		}
+		return harvested;
+	}
+	
 	public boolean hasVillage() {
 		return this instanceof Village;
 	}
--- VillageCraft/src/resources/Material.java
+++ VillageCraft/src/resources/Material.java
@@ -1,5 +1,39 @@
 package resources;
 
 public class Material extends Item {
-
+	
+	//*
+	//to be used as a sort of pseudo single-instance class maybe?
+	private int type;
+	private Material(int type)
+	{
+		this.type = type;
+	}
+	
+	private static final int RSRCE_FOOD = 0, RSRCE_LUMBER = 1;
+	public static Material lumber = new Material(RSRCE_LUMBER);
+	public static Material food = new Material(RSRCE_FOOD);
+	/*/
+	public static final int RSRCE_NOTHING = 0, RSRCE_LUMBER = 1;
+	public final int type;
+	private int quantity;
+	public Material(int type, int quantity)
+	{
+		this.type = type;
+		this.quantity = quantity;
+	}
+	
+	public int getQuantity()
+	{
+		return quantity;
+	}
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+	public int getType()
+	{
+		return type;
+	}
+	//*/
 }
