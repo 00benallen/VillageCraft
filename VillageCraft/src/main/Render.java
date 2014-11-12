880fd7b5cde64637fcfe7a1b0777206b25c7771b
Daniel McIntosh <Daniel@DanielMcIntosh.ca>
2014-11-12 09:58:25 -0500

    implemented manual double buffering

--- VillageCraft/src/main/Render.java
+++ VillageCraft/src/main/Render.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class Render implements Runnable {
 	Graphics2D g;
@@ -36,15 +37,18 @@ public class Render implements Runnable {
 	}
 	
 	public void draw() {
-		drawGround();
-		drawWorld();
+		BufferedImage buffer = new BufferedImage(GraphicsMain.WIDTH, GraphicsMain.HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D gBuffer = (Graphics2D)buffer.getGraphics(); 
+		drawGround(gBuffer);
+		drawWorld(gBuffer);
+		g.drawImage(buffer, 0, 0, GraphicsMain.WIDTH, GraphicsMain.HEIGHT, null);
 	}
 	
-	public void drawWorld() {
+	public void drawWorld(Graphics2D g) {
 		g.drawImage(Main.getLoadedWorld().draw(), 0, 0, null);
 	}
 
-	public void drawGround() {
+	public void drawGround(Graphics2D g) {
 		g.setColor(new Color(1, 166, 17));
 		g.fillRect(0, 0, GraphicsMain.WIDTH, GraphicsMain.HEIGHT);
 	}	
